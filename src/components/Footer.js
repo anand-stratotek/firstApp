72305d64efbde179774c4c478cdff696268a3905
anand-stratotek <anand.gautam@stratotekcg.com>
2021-08-22 19:29:16 +0530

    formChange

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,48 @@
 
 */
 
-import React, { Component } from 'react';
-import Header from './Header';
+import React from 'react';
 
-class Footer extends Component {
-  render() {
-    return <div className='footer'>This is my class based component</div>;
-  }
-}
+const Footer = () => {
+  return (
+    <div class='footer-basic'>
+      <footer>
+        <div class='social'>
+          <a href='#'>
+            <i class='icon ion-social-instagram'></i>
+          </a>
+          <a href='#'>
+            <i class='icon ion-social-snapchat'></i>
+          </a>
+          <a href='#'>
+            <i class='icon ion-social-twitter'></i>
+          </a>
+          <a href='#'>
+            <i class='icon ion-social-facebook'></i>
+          </a>
+        </div>
+        <ul class='list-inline'>
+          <li class='list-inline-item'>
+            <a href='#'>Home</a>
+          </li>
+          <li class='list-inline-item'>
+            <a href='#'>Services</a>
+          </li>
+          <li class='list-inline-item'>
+            <a href='#'>About</a>
+          </li>
+          <li class='list-inline-item'>
+            <a href='#'>Terms</a>
+          </li>
+          <li class='list-inline-item'>
+            <a href='#'>Privacy Policy</a>
+          </li>
+        </ul>
+        <p class='copyright'>Company Name © 2018</p>
+      </footer>
+      ;
+    </div>
+  );
+};
 
 export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,77 +12,109 @@ useRef()
 */
 
 function Main() {
-  const initialValues = {
-    name: '',
-    email: '',
-    dateofbirth: '',
-    mobileNumber: '',
-    city: '',
-  };
+  // const initialValues = {
+  //   name: '',
+  //   email: '',
+  //   dateofbirth: '',
+  //   mobileNumber: '',
+  //   city: '',
+  // };
 
-  //   console.log('INITIAL VALUES=>', initialValues);
+  // //   console.log('INITIAL VALUES=>', initialValues);
 
-  const [formValues, setFormValues] = useState(initialValues);
-  console.log('formValues=>', formValues);
+  // const [formValues, setFormValues] = useState(initialValues);
+  // console.log('formValues=>', formValues);
 
-  function onFormValueChange(event) {
-    // console.log('YE CALL HUA');
-    // console.log('event=>', event.target.value);
-    //...formValues==> jo bhi mera purana values hai usko copy kar lo
-    // console.log('NAME=>', event.target.name);
-    setFormValues({ ...formValues, [event.target.name]: event.target.value });
+  // function onFormValueChange(event) {
+  //   // console.log('YE CALL HUA');
+  //   // console.log('event=>', event.target.value);
+  //   //...formValues==> jo bhi mera purana values hai usko copy kar lo
+  //   // console.log('NAME=>', event.target.name);
+  //   setFormValues({ ...formValues, [event.target.name]: event.target.value });
+  // }
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [city, setCity] = useState('');
+  const [dateofbirth, setDateOfBirth] = useState('');
+
+  function onFormSubmit(e) {
+    console.log('FORM SUBMIT');
+    e.preventDefault();
+  }
+
+  function onNameChange(e) {
+    setName(e.target.value);
   }
 
   return (
     <div className='registration-form'>
-      <h1>REGISTRATION FORM:-</h1>
-      <div>
-        <div>
-          <label>NAME:</label>
-          <input
-            name='name'
-            type='text'
-            value={formValues.name}
-            onChange={onFormValueChange}
-          />
+      <h1>REGISTRATION FORM</h1>
+      <form onSubmit={onFormSubmit} id='form-id-001'>
+        <div className='form-list'>
+          <div className='form-element'>
+            <label>NAME:</label>
+            <input
+              name='name'
+              type='text'
+              id='name'
+              // value={name}
+              placeholder='Please enter your name'
+              onChange={onNameChange}
+            />
+          </div>
+          <div className='form-element'>
+            <label>EMAIL:</label>
+            <input
+              name='email'
+              type='text'
+              id='email'
+              // value={email}
+              // onChange={onFormValueChange}
+              placeholder='Please enter your emailId'
+            />
+          </div>
+          <div className='form-element'>
+            <label>DOB:</label>
+            <input
+              name='dateofbirth'
+              type='date'
+              id='dob'
+              // value={dateofbirth}
+              // onChange={onFormValueChange}
+              placeholder='Date of Birth'
+            />
+          </div>
+          <div className='form-element'>
+            <label>MOBILE:</label>
+            <input
+              name='mobileNumber'
+              type='number'
+              id='mobile'
+              // value={mobileNumber}
+              // onChange={onFormValueChange}
+              placeholder='Enter your mobile number'
+            />
+          </div>
+          <div className='form-element'>
+            <label>CITY:</label>
+            <input
+              name='city'
+              id='city'
+              type='text'
+              // value={city}
+              // onChange={onFormValueChange}
+              placeholder='Your City'
+            />
+          </div>
+          {/* <div className='form-element'>
+            <label> Details you have entered:-</label>
+            {JSON.stringify(formValues)}
+          </div> */}
         </div>
-        <div>
-          <label>EMAIL:</label>
-          <input
-            name='email'
-            type='text'
-            value={formValues.email}
-            onChange={onFormValueChange}
-          />
-        </div>
-        <div>
-          <label>DOB:</label>
-          <input
-            name='dateofbirth'
-            type='date'
-            value={formValues.dateofbirth}
-            onChange={onFormValueChange}
-          />
-        </div>
-        <div>
-          <label>MOBILE:</label>
-          <input
-            name='mobileNumber'
-            type='number'
-            value={formValues.mobileNumber}
-            onChange={onFormValueChange}
-          />
-        </div>
-        <div>
-          <label>CITY:</label>
-          <input
-            name='city'
-            type='text'
-            value={formValues.city}
-            onChange={onFormValueChange}
-          />
-        </div>
-      </div>
+        <button type='submit'> FORM SUBMIT</button>
+      </form>
     </div>
   );
 }
